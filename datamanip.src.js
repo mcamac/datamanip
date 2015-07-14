0f1fd5457f5f34a5cdca2ed632ef3bfe66d528d9
Martin Camacho <martin@mcamac.com>
2015-07-14 16:45:48 -0400

    add types

--- datamanip.src.js
+++ datamanip.src.js
@@ -1,6 +1,7 @@
 // These are not meant to be super fast - we can improve as necessary.
 var R = require('ramda')
 
+// {k: String || (v -> v')} -> [{k: v}]-> [{k: v}]
 export var mapKeys = R.curry(function (keyMap, arr) {
   return arr.map(pt => {
     var mapped = {}
@@ -15,18 +16,22 @@ export var mapKeys = R.curry(function (keyMap, arr) {
   })
 })
 
+// {k: String || (v -> v')} -> [{k: v}]-> [{k: v}]
 export var extendMapKeys = R.curry(function (keyMap, arr) {
   return R.zipWith(R.merge, arr, mapKeys(keyMap, arr))
 })
 
+// Number -> Number -> [int]
 export var irange = R.curry(function (m, n) {
   return R.range(m, Math.floor(n + 1))
 })
 
+// (a -> [k, v]) -> [a] -> {k: v}
 export var objFromItemFn = R.curry(function (fn, keys) {
   return R.fromPairs(keys.map(fn))
 })
 
+// (k -> v) -> [k] -> {k: v}
 export var objFromValFn = R.curry(function (fn, keys) {
   return objFromItemFn(k => [k, fn(k)], keys)
 })
