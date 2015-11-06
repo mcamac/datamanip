5fbdfdd9a2e8fb1044b61cc2589339986b7f5a41
Martin Camacho <martin@mcamac.com>
2015-11-06 11:00:13 -0500

    Add .eslintrc

--- datamanip.js
+++ datamanip.js
@@ -1,47 +1,48 @@
 // These are not meant to be super fast - we can improve as necessary.
-'use strict'
+'use strict';
 
-exports.__esModule = true
-var R = require('ramda')
+exports.__esModule = true;
+var R = require('ramda');
 
 // {k: String || (v -> v')} -> [{k: v}]-> [{k: v}]
 var mapKeys = R.curry(function (keyMap, arr) {
   return arr.map(function (pt) {
-    var mapped = {}
+    var mapped = {};
     _.forEach(keyMap, function (val, key) {
       if (typeof val === 'string') {
-        mapped[key] = pt[val]
+        mapped[key] = pt[val];
       } else {
-        mapped[key] = val(pt)
+        mapped[key] = val(pt);
       }
-    })
-    return mapped
-  })
-})
+    });
+    return mapped;
+  });
+});
 
-exports.mapKeys = mapKeys
+exports.mapKeys = mapKeys;
 // {k: String || (v -> v')} -> [{k: v}]-> [{k: v}]
 var extendMapKeys = R.curry(function (keyMap, arr) {
-  return R.zipWith(R.merge, arr, mapKeys(keyMap, arr))
-})
+  return R.zipWith(R.merge, arr, mapKeys(keyMap, arr));
+});
 
-exports.extendMapKeys = extendMapKeys
+exports.extendMapKeys = extendMapKeys;
 // Number -> Number -> [int]
 var irange = R.curry(function (m, n) {
-  return R.range(m, Math.floor(n + 1))
-})
+  return R.range(m, Math.floor(n + 1));
+});
 
-exports.irange = irange
+exports.irange = irange;
 // (a -> [k, v]) -> [a] -> {k: v}
 var objFromItemFn = R.curry(function (fn, keys) {
-  return R.fromPairs(keys.map(fn))
-})
+  return R.fromPairs(keys.map(fn));
+});
 
-exports.objFromItemFn = objFromItemFn
+exports.objFromItemFn = objFromItemFn;
 // (k -> v) -> [k] -> {k: v}
 var objFromValFn = R.curry(function (fn, keys) {
   return objFromItemFn(function (k) {
-    return [k, fn(k)]
-  }, keys)
-})
-exports.objFromValFn = objFromValFn
+    return [k, fn(k)];
+  }, keys);
+});
+exports.objFromValFn = objFromValFn;
+
